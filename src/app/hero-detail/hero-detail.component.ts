cd5719086784248a7d4784fe5d85ccbbb722c85d
Victor Ribeiro <victor.ribeiro@comlink.com.br>
2023-10-04 13:23:07 -0300

    Adicionando estilização ao escolher um guitarrista

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.scss']
+})
+export class HeroDetailComponent {
+
+}
